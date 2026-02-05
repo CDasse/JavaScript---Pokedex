8d79b5e718e64736ea6e78e2b5b0fbf8306445be
CDasse <clemence.dasse@yahoo.fr>
2026-02-05 19:13:28 +0100

    Feat: test deployement

--- my-first-pokedex-coda/src/web-component/pokemon-card.ts
+++ my-first-pokedex-coda/src/web-component/pokemon-card.ts
@@ -14,7 +14,7 @@ class PokemonCard extends HTMLElement {
         this.innerHTML = `<div class="pokemon-card" data-id-pokemon="${id}">
              <h3>#${id}</h3>
              <img src=${img} alt="Image of ${name}" 
-                height="100" onerror="this.src='/src/img/favicon.png'; this.onerror=null;"> 
+                height="100" onerror="this.src='${logoUrl}'; this.onerror=null;"> 
              <div class="pokemon-name">${name}</div>
              </div>`
 
